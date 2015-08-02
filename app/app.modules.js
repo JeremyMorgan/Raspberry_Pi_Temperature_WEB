0ba91848d195819be0690696a7746c08774105ca
Jeremy Morgan <jeremymorgan@gmail.com>
2015-08-02 15:27:45 -0700

    removed old files

--- app/app.modules.js
+++
@@ -1,9 +0,0 @@
-/**
- * Load modules for application
- */
-var angularstrapApp = angular.module('angularstrapApp', ['ui.router', 'angularstrapControllers', 'angularstrapServices']);
-
-// set our API host
-angularstrapApp.constant('APIHOST', 'http://localhost:12017');
-
-angularstrapApp.controller('HomeController', function( HomeController ){});
